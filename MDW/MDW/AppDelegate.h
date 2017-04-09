bbdeb40dca1d34dc386b9c93f572109c14678cd0
ToqaEid <toqa.eid@gmail.com>
2017-04-09 19:54:58 +0100

    project structure

---
+++ MDW/MDW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MDW
+//
+//  Created by JETS on 4/9/17.
+//  Copyright © 2017 JETS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

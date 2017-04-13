6f1d0d5a6076b5a236244c40c8155107132668dd
ToqaEid <toqa.eid@gmail.com>
2017-04-13 09:40:17 +0100

    initial storyboard views

--- MDW/MDW/ViewControllers/SideMenuTableViewController/JETSSideMenu.m
+++ MDW/MDW/ViewControllers/SideMenuTableViewController/JETSSideMenu.m
@@ -8,32 +8,43 @@
 
 #import "JETSSideMenu.h"
 
-@implementation JETSSideMenu
+@implementation JETSSideMenu{
+    NSArray * cellsItems;
+
+}
 
 
 
 -(void)viewDidLoad{
+    cellsItems = @[@"Agenda", @"MyAgenda", @"Speakers", @"Exhibitors", @"Profile", @"Logout"];
+    
+    self.tableView.backgroundView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"leftSideMenuBackground.png"]];
+    
 }
 
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
 
-    return 2;
+    return 1;
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     
-    if(section == 1){
-        return 2;
-    }else if (section == 2){
-        return 3;
-    }
-    return 1;
+//    if(section == 1){
+//        return 2;
+//    }else if (section == 2){
+//        return 3;
+//    }
+//    return 1;
+    return cellsItems.count;
 }
 
--(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
-    static NSString *cellIdentifier = @"Cell";
+    NSString *cellIdentifier = [cellsItems objectAtIndex:indexPath.row];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
     
+    
+    return cell;
 
 }
 @end
